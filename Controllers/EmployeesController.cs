44b574f2916dcba7c31d55b2f9b157da46a655a2
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-01 14:04:50 +0700

    Вынесение данных из контроллера

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -12,33 +13,7 @@ namespace WebStore.Controllers
     //[Route("Staff")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Surname = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Age = 50
-            },
-            new Employee
-            {
-                Id = 2,
-                Surname = "Петров",
-                FirstName = "Петр",
-                Patronymic = "Петрович",
-                Age = 25
-            },
-            new Employee
-            {
-                Id = 3,
-                Surname = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 30
-            }
-        };
+        private static readonly List<Employee> __Employees = TestData.Employees;
 
         //[Route("List")]
         public IActionResult Index()
