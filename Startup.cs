f62631b7f1aa6a012c57b934240d7136b80b41dd
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-01 13:46:23 +0700

    Промежуточное ПО

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 namespace WebStore
 {
@@ -28,18 +29,34 @@ namespace WebStore
      
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Здесь подключается все промежуточное ПО
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();    //Отвечает за отображение страницы с эксепшенами. Если отсутствует, то просто возвращается код ошибки 500
 
                 app.UseBrowserLink();
             }
 
+            //это промежуточное ПО отвечает за возврат статического содержимого
             app.UseStaticFiles();
             app.UseDefaultFiles();
 
-            app.UseRouting();            
+            //Это маршрутизация
+            app.UseRouting();
 
+            //показывает рекламную страницу MVC
+            app.UseWelcomePage("/MVC");
+
+            //Вызвать свое промежуточное ПО
+            app.Use(async (context, next) => 
+            {
+                Debug.WriteLine($"Request to {context.Request.Path}");
+                await next(); //Если не вызвать next() конвейер прервется
+            });
+
+            //app.UseMiddleware<>();
+
+            //Маппинг
             app.UseEndpoints(endpoints =>
             {                
 
