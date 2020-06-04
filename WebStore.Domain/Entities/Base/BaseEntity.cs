c781b30b3ceb037c5a8de34b7095d946388bf9a0
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-04 17:29:09 +0700

    Базовые классы сущностей

---
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+
+    public abstract class NamedEntity : BaseEntity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
