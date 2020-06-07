19f43eb55033cdeaf9434f9771b5e53f0269132c
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-08 01:15:59 +0700

    Реализация метода извлечения товаров из каталога с фильтром

--- WebStore/Infrastructure/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.DotNet.Cli.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Base;
+using Product = WebStore.Domain.Entities.Product;
 
 namespace WebStore.Infrastructure.Interfaces
 {
@@ -11,5 +14,7 @@ namespace WebStore.Infrastructure.Interfaces
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
     }
 }
--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Base;
 using WebStore.Infrastructure.Interfaces;
 
@@ -11,6 +12,18 @@ namespace WebStore.Infrastructure.Services
     public class InMemoryProductData : IProductData
     {
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            var query = TestData.Products;
+
+            if (Filter?.SectionId != null)
+                query = query.Where(product => product.SectionId == Filter.SectionId);
+            if (Filter?.BrandId != null)
+                query = query.Where(product => product.BrandId == Filter.BrandId);
+           
+            return query;
+        }
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
     }
