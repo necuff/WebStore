179c33e4da84d1b4f4242f0778cddb9c37d3f616
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-16 23:22:53 +0700

    Заготовка сервиса работы с сотрудниками в БД

---
+++ WebStore/Infrastructure/Services/InSQL/SqlEmployeesData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services.InSQL
+{
+    public class SqlEmployeesData : IEmployeesData
+    {
+        private readonly WebStoreDB _db;
+
+        public SqlEmployeesData(WebStoreDB db) => _db = db; 
+
+        public int Add(Employee Employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Employee Employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
