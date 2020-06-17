29f52bcb90dd928ccd041a1431fd268d8f78d02c
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-18 01:48:13 +0700

    Заготовка метода Login

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -24,7 +24,7 @@ namespace WebStore.Controllers
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterUserViewModel Model)
         {
-            if (ModelState.IsValid) return View(Model);
+            if (!ModelState.IsValid) return View(Model);
 
             var user = new User
             {
@@ -43,11 +43,24 @@ namespace WebStore.Controllers
                 ModelState.AddModelError(string.Empty, error.Description);
             }
             return View(Model);
-        } 
+        }
         #endregion
 
 
-        public IActionResult Login() => View();
+        #region Login user
+        
+        public IActionResult Login() => View(new LoginViewModel()); 
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (ModelState.IsValid) return View(Model);
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        #endregion
 
         public IActionResult Logout() => View();
 
