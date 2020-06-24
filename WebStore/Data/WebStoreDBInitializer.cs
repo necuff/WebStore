248c9fbad8c5937ced72b4bf1b364ecf20af1a37
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-24 17:04:11 +0700

    Инициализация пользователя Администратор и ролей Администратор и пользователь

--- WebStore/Data/WebStoreDBInitializer.cs
+++ WebStore/Data/WebStoreDBInitializer.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
 
-        public WebStoreDBInitializer(WebStoreDB db) { _db = db; }
+        public WebStoreDBInitializer(WebStoreDB db, UserManager<User> userManager, RoleManager<Role> roleManager) 
+        { 
+            _db = db;
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+        }
 
         public void Initialize()
         {
@@ -25,29 +34,47 @@ namespace WebStore.Data
             db.Migrate();   //аналог update-database
 
 
-            if (!_db.Employees.Any())
-                using (db.BeginTransaction())
-                {
-                    var employees = TestData.Employees.ToList();
-                    /*
-                    foreach (var employee in employees)
-                        employee.Id = 0;
-                    */
+            InitializeEmployees();
 
-                    employees.ForEach(e => e.Id = 0); 
+            InitializeProducts();
 
-                    _db.Employees.AddRange(employees);
-                }            
-                    
+            InitializeIdentityAsync().Wait();
+        }
+
+        private void InitializeEmployees()
+        {
+            var db = _db.Database;
+
+            if (!_db.Employees.Any()) return;
+            
+            using (db.BeginTransaction())
+            {
+                var employees = TestData.Employees.ToList();
+                /*
+                foreach (var employee in employees)
+                    employee.Id = 0;
+                */
+
+                employees.ForEach(e => e.Id = 0);
+
+                _db.Employees.AddRange(employees);
+            }
+            
+                
+        }
+
+        private void InitializeProducts()
+        {
+            var db = _db.Database;
 
             if (_db.Products.Any()) return;
 
             using (db.BeginTransaction())
             {
                 _db.Sections.AddRange(TestData.Sections);
-                
+
                 //Чтобы вручную править ключи в БД
-                db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[ProductSection] ON");                
+                db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[ProductSection] ON");
                 _db.SaveChanges();
                 db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[ProductSection] OFF");
 
@@ -74,6 +101,31 @@ namespace WebStore.Data
                 db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] OFF");
 
                 tran.Commit();
+            }
+        }
+
+
+        private async Task InitializeIdentityAsync()
+        {
+            if (!await _roleManager.RoleExistsAsync(Role.Administrator))
+                await _roleManager.CreateAsync(new Role { Name = Role.Administrator });
+
+            if (!await _roleManager.RoleExistsAsync(Role.User))
+                await _roleManager.CreateAsync(new Role { Name = Role.User });
+
+            if(await _userManager.FindByNameAsync(User.Administrator) is null)
+            {
+                var admin = new User { UserName = User.Administrator };
+
+                var create_result = await _userManager.CreateAsync(admin, User.DefaultAdminPassword);
+
+                if (create_result.Succeeded)
+                    await _userManager.AddToRoleAsync(admin, Role.Administrator);
+                else
+                {
+                    var errors = create_result.Errors.Select(e => e.Description);
+                    throw new InvalidOperationException($"Ошибка при созании пользователя Администратор: {string.Join(",", errors)}");
+                }
             }
         }
     }
