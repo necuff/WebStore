fe1fc16ac163e2bd0b2638b110444b1b148f85eb
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-24 22:55:55 +0700

    Модели корзины + модель-представление корзины

---
+++ WebStore/ViewMoodel/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewMoodel
+{
+    public class CartViewModel
+    {
+        public Dictionary<ProductViewModel, int> Items { get; set; } = new Dictionary<ProductViewModel, int>();
+
+        public int ItemsCount => Items?.Sum(item => item.Value) ?? 0;
+    }
+}
