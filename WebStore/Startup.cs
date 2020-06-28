3d767451540b12b3b1e2d559d20366301974ccb6
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-28 17:57:22 +0700

    Заготовка области администратора

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -117,7 +117,11 @@ namespace WebStore
 
             //Маппинг
             app.UseEndpoints(endpoints =>
-            {                
+            {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
 
                 endpoints.MapControllerRoute(
                     name: "default",
