3d75a313599bae40deb9256580229e602cf41992
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-28 18:23:13 +0700

    Обязательно! ОБЯЗАТЕЛЬНО добавляем атрибут на контроллер для области

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CatalogController : Controller
     {
         public IActionResult Index()
