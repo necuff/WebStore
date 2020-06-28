f97cd90b2d8df9b760312fb6f48878aa3bc79b42
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-28 22:27:43 +0700

    Заготовка сервиса заказов

---
+++ WebStore/Infrastructure/Services/InSQL/SqlOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Services.InSQL
+{
+    public class SqlOrderService : IOrderService
+    {
+
+    }
+}
