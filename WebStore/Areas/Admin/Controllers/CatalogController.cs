47c47ab079fc5b99d67be51c7c67e1f433640626
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-28 19:28:38 +0700

    Доработка макета области администратора

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,17 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CatalogController : Controller
     {
+        private readonly IProductData _productData;
+
+        public CatalogController(IProductData productData) => _productData = productData;
+
         public IActionResult Index()
         {
-            return View();
+            return View(_productData.GetProducts());
         }
+
+        public IActionResult Edit(int? id)
+        {
+            var product = id is null ? new Product() : _productData.GetProductById((int)id);
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productData.GetProductById(id);
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName(nameof(Delete))]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
 }
