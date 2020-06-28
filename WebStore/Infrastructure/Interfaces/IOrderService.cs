3eb2da24d5077dc148aef59f9568c348632e2b38
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-28 23:02:59 +0700

    Сервис заказов

--- WebStore/Infrastructure/Interfaces/IOrderService.cs
+++ WebStore/Infrastructure/Interfaces/IOrderService.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Orders;
+using WebStore.ViewMoodel;
 
 namespace WebStore.Infrastructure.Interfaces
 {
     interface IOrderService
     {
+        Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel);
+
+        Task<IEnumerable<Order>> GetUserOrders(string UserName);
+
+        Task<Order> GetOrderById(int id);
     }
 }
