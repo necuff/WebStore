5d8d2c5a94353ecf331d400c2e2a41b27a4c56ec
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-06-30 01:56:10 +0700

    MVC-контроллер для визуализации работы тестовой службы WebAPI

---
+++ UI/WebStore/Controllers/WebApiTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebApiTestController : Controller
+    {
+        private readonly IValueService valueService;
+
+        public WebApiTestController(IValueService ValueService) => valueService = ValueService;
+
+        public IActionResult Index()
+        {
+            var values = valueService.Get();
+            return View(values);
+        }
+    }
+}
